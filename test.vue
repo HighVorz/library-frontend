0f168cc93f77229ce969bf282d5650789486a76c
Ex-monsters <2055318980@qq.com>
2023-12-27 21:35:55 +0800

    add test

---
+++ test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- Your HTML content goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    // Your Vue component logic goes here
+}
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
