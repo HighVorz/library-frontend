73dc1f910f3c740ba6f97964e50d740ad6204641
Ex-monsters <2055318980@qq.com>
2023-12-29 12:22:27 +0800

    Add pop-up window

--- src/view/administer.vue
+++ src/view/administer.vue
@@ -2,7 +2,37 @@
     <div class="header">
         <h1>欢迎来到图书管理系统</h1>
     </div>
-    
+    <div class="choose">
+        <form class="form-inline">
+            <div class="form-group">
+                <label for="name">书名:</label>
+                <input type="text" id="name" class="form-control">
+                <label for="au">作者:</label>
+                <input type="text" id="au" class="form-control">
+                <label for="num">数量:</label>
+                <input type="number" id="num" class="form-control">
+                <label for="pub">出版社:</label>
+                <input type="text" id="pub" class="form-control">
+                <label for="money">单价:</label>
+                <input type="number" id="money" class="form-control">
+                <input type="button" id="addBook" class="add btn btn-primary" value="添加图书" />
+            </div>
+        </form>
+    </div>
+    <div class="leader">
+        <h2>可管理图书列表</h2>
+    </div>
+    <div class="container">
+        <div class="book" v-for="item in bookTotal">
+            <h3>《{{ item.bookName }}》</h3>
+            <img :src="item.bookRef" @click="borrowBook(item.bookName)">
+            <p>作者:{{ item.bookAuthor }}</p>
+            <p>出版社:{{ item.bookPublisher }}</p>
+            <p>出版日期:{{ item.bookDate }}</p>
+            <p>可借阅状态:{{ item.bookStatus }}</p>
+        </div>
+    </div>
+    <div class="search-mask" style="display: block;overflow: hidden; position: fixed;top: 0%;right: 0%;bottom: 0%;left: 0%;"></div>
 </template>
 
 <script>
@@ -21,5 +51,60 @@ export default {
     background: url('../assets/img/cloud.jpg') center/cover no-repeat;
     color: rgb(36, 93, 116);
     padding: 15px;
+    text-align: center;
 }
+
+.form-inline {
+    background-color: rgba(42, 145, 236,0.8);
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+
+.form-inline:hover {
+    background-color: rgba(42, 145, 236,1);
+}
+
+.form-group {
+    box-sizing: border-box;
+    font-weight: bold;
+    margin-left: 9%;
+    margin-right:9%;
+}
+
+.form-control {
+    width: 12%;
+    height: 20px;
+    font-weight: bold;
+    margin: 5px;
+    border-radius: 4px;
+    outline:none;
+    border: none;
+    background-color: #f2f2f2;
+    opacity: 0.8;
+}
+
+.form-control:focus {
+    opacity: 1;
+}
+
+#addBook {
+    margin-left: 20px;
+    height: 20px;
+    width: 100px;
+    background-color: rgba(255, 242, 0, 0.75);
+    font-weight: bold;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+}
+
+#addBook:hover {
+    background-color: rgba(255, 242, 0, 1);
+}
+
+.container {
+    background: url('/assets/img/lib_admin.jpg') center/cover no-repeat;
+    background-attachment: fixed;
+}
+
 </style>
--- src/view/book_detail.vue
+++ src/view/book_detail.vue
@@ -1,7 +1,196 @@
 <template>
-    <h1>Book_detail</h1>
+    <h1>书名:</h1>
+    <div>
+        <button @click="showModal = true">搜索一下</button>
 
+        <div v-if="showModal" class="modal">
+            <div class="search-container">
+                <form class="search-form">
+                    <div class="form-group">
+                        <label for="name">书名:</label>
+                        <input type="text" id="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="au">作者:</label>
+                        <input type="text" id="au" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="num">数量:</label>
+                        <input type="number" id="num" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="pub">出版社:</label>
+                        <input type="text" id="pub" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="money">单价:</label>
+                        <input type="number" id="money" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button class="add">查找图书</button>
+                        <button @click="showModal = false" class="quit">关闭</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <table>
+        <tr>
+            <td>书名</td>
+            <td>作者</td>
+            <td>价格</td>
+            <td>出版社</td>
+            <td>出版时间</td>
+            <td>添加&nbsp&nbsp&nbsp删除</td>
+        </tr>
+        <tr>
+            <td>书名</td>
+            <td>作者</td>
+            <td>价格</td>
+            <td>出版社</td>
+            <td>出版时间</td>
+            <td>添加&nbsp&nbsp&nbsp删除</td>
+        </tr>
+        <tr>
+            <td>书名</td>
+            <td>作者</td>
+            <td>价格</td>
+            <td>出版社</td>
+            <td>出版时间</td>
+            <td>添加&nbsp&nbsp&nbsp删除</td>
+        </tr>
+    </table>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            showModal: false
+        }
+    },
+    watch: {
+        showModal(val) {
+            if (val) {
+                document.body.style.overflow = 'hidden'
+            } else {
+                document.body.style.overflow = 'auto'
+            }
+        }
+    }
+}
 </script>
+
+<style scpoed>
+table {
+    border-collapse: collapse;
+    width: 100%;
+    font-family: Arial, sans-serif;
+}
+
+td {
+    border: 1px solid black;
+    padding: 10px;
+}
+
+tr:nth-child(even) {
+    background-color: #f2f3ea;
+}
+
+tr:hover {
+    background-color: #ddd;
+}
+
+.modal {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+
+.search-container {
+    width: 100%;
+    margin: auto;
+    border: none;
+    margin-top: 20%;
+}
+
+.search-form {
+    width: 30%;
+    margin: auto;
+    background-color: #fefefe;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border-radius: 20px;
+    padding: 20px;
+}
+
+.form-group {
+    align-items: center;
+    margin-bottom: 10px;
+}
+
+.form-group label {
+    display: inline-block;
+    width: 60px;
+    font-size: 16px;
+    font-weight: bold;
+    margin-right: 5px;
+    margin-top: 8px;
+    margin-bottom: 3px;
+    color: rgb(56, 56, 56);
+}
+
+.form-group input {
+    width: 68%;
+    height: 32px;
+    padding: 0 12px;
+    margin-right: 5px;
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0 5px 10px 5px rgba(47, 45, 45, 0.08);
+    font-size: 16px;
+    background-color: #e9e7ee;
+    font-weight: 600;
+}
+
+.add {
+    width: 30%;
+    padding: 10px;
+    border-radius: 20px;
+    font-size: 16px;
+    font-weight: bold;
+    color: white;
+    margin-top: 10px;
+    margin-left:5%;
+    margin-right:30%;
+    background-color: #007bff;
+    border: none;
+    cursor: pointer;
+}
+
+.add:hover {
+    background-color: #0056b3;
+}
+
+.quit {
+    width: 30%;
+    padding: 10px;
+    border-radius: 20px;
+    font-size: 16px;
+    font-weight: bold;
+    color: white;
+    margin-top: 10px;
+    background-color: #dc3545;
+    border: none;
+    cursor: pointer;
+}
+
+.quit:hover {
+    background-color: #b02a37;
+}
+</style>
