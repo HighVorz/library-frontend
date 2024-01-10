0f6d9b4f75393ba035d15636b8d3c1de3cb91d8f
HighVorz <HighVorz@outlook.com>
2024-01-10 20:16:53 +0800

    fix missing

--- src/view/userspace.vue
+++ src/view/userspace.vue
@@ -138,20 +138,9 @@ function renewBook(id) {
 </script>
 
 <style>
-.avatar {
-    vertical-align: middle;
-}
-
-.circle {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    border-width: 1px;
-    border-style: solid;
-}
 
 .bg-image {
-    background-image: url("/assets/img/lib_register.png");
+    background-image: url("/assets/img/claim.jpg");
     background-repeat: no-repeat;
     background-size: cover;
     position: fixed;
@@ -160,5 +149,179 @@ function renewBook(id) {
     width: 100%;
     height: 100%;
     z-index: -1;
+}
+
+/* 
+.z-container {
+    width: 100%;
+    height: 100%;
+    background: url("/assets/img/claim.jpg") center/cover no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+} */
+
+.user-space {
+    display: flex;
+    width: 900px;
+    height: 550px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 10px;
+}
+
+.selected {
+    background-color: #ddd;
+}
+
+.sidebar {
+    width: 20%;
+    border-right: 1px solid #ddd;
+}
+
+.sidebar ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+.sidebar li {
+    cursor: url("/assets/img/alternate.ico"), auto;
+    padding: 10px;
+}
+
+.sidebar li:hover {
+    background-color: #ddd;
+}
+
+.basicinfo {
+    text-align: center;
+}
+
+.main {
+    width: 80%;
+    padding: 20px;
+}
+
+.avatar-section {
+    text-align: center;
+}
+
+.avatar-img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+}
+
+.modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 30%;
+    border-radius: 10px;
+    text-align: center;
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    cursor: url("/assets/img/alternate.ico"), auto;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+}
+
+input[type="file"] {
+    margin-top: 10px;
+}
+
+.styled-table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.styled-table th,
+.styled-table td {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+.styled-table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: rgba(76, 175, 80, 0.8);
+    color: white;
+}
+
+.styled-table tr:nth-child(even) {
+    background-color: rgba(242, 242, 242, 0.7);
+}
+
+.styled-table tr:hover {
+    background-color: #ddd;
+}
+
+.styled-table button {
+    margin-right: 10px;
+    color: white;
+    border: none;
+    border-radius: 8px;
+    padding: 5px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 8px;
+}
+
+.styled-table button:hover {
+    animation: bounce 0.3s forwards;
+}
+
+.retbk {
+    background-color: rgba(231, 185, 19, 0.7);
+}
+
+.retbk:hover {
+    background-color: rgba(231, 185, 19, 1);
+}
+
+.kepbk {
+    background-color: rgba(139, 26, 157, 0.7);
+}
+
+.kepbk:hover {
+    background-color: rgba(139, 26, 157, 1);
+}
+
+@keyframes bounce {
+    0% {
+        transform: translateY(0);
+    }
+
+    100% {
+        transform: translateY(-5px);
+    }
 }
 </style>
